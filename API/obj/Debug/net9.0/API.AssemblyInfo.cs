b55a073c1073f9ac9fcc79a467ee6f676600810c
Yusuf Okan Çevik <81883631+xHolland0@users.noreply.github.com>
2025-06-28 02:31:30 +0300

    Added New Client Side Functions

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcd930d3445b0636bab2da25005d37bfb44a5c00")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,8 +1,70 @@
-function App() {
+import { useState } from "react"
 
+const Products = [
+  {id: 1, name: "product1", price: 100,IsActive:true},
+  {id:2, name: "product2", price: 200,IsActive:false},
+  {id:3, name: "product3", price: 300,IsActive:true}
+]
+
+
+
+
+function App() {
   return (
-    <h1>React.js</h1>
+    <>
+    <Header></Header>
+    <ProductList></ProductList>
+    </>
   )
+}
+
+function Header(){
+  return(
+    <h1>Header</h1>
+  )
+}
+
+function ProductList(){
+
+  const[Products, setProducts] = useState([
+  {id: 1, name: "product1", price: 100,IsActive:true},
+  {id:2, name: "product2", price: 200,IsActive:false},
+  {id:3, name: "product3", price: 300,IsActive:true},
+  ]);
+
+  function addProduct(){
+    setProducts([...Products,{id:Date.now(), name: "product4", price: 400,IsActive:true}])
+  }
+
+  console.log("Render..");
+  return(
+    <>
+    <h2>Product List</h2>
+
+    {Products.map(p=>(
+      <Product key={p.id} product={p} />
+    ))}
+
+    <button onClick={addProduct}>Add Product</button>
+
+    </>
+  )
+}
+
+function Product(props: any){
+  return(
+    <>
+  {props.product.IsActive ?(
+    <div>
+    <h3>{props.product.name}</h3> 
+    <p>{props.product.price}</p>
+    </div>
+  ): <p>Ürün Satışta Değil</p>
+  }
+    </>
+
+
+  );
 }
 
 export default App
